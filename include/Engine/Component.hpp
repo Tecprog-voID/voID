5a6224f40ae721c4efb3d216dc8241cfbb23ad14
Diego França <diegobmf@gmail.com>
2017-11-29 10:46:41 -0200

    Second application of clean code technique Applied in Component and GraphicsSystem classes.

--- include/Engine/Component.hpp
+++ include/Engine/Component.hpp
@@ -20,24 +20,32 @@ class Component {
 
         // constructor and destructor
         Component(GameObject *owner, ComponentType type);
+
         ~Component();
         // getters and setters
         virtual inline ComponentType GetType() final {
             return m_type;
         };
+
         virtual inline void SetOwner(GameObject *gameObject) final {
             m_owner = gameObject;
         };
+
         virtual inline GameObject *GetOwner() {
             return m_owner;
         };
+
         // unoverridable update
         virtual void Update() final;
+
         // method for getting the component name
         virtual std::string GetComponentName() = 0;
+
         // Start method
         virtual void Start(){};
+
         virtual void FixedComponentUpdate(){};
+
         virtual void ComponentUpdate(){};
 
         // Component type.
